5ea518fec0e6842ad44c7c0e3e03d49241e55363
Widadullahi <alagbewidad814@gmail.com>
2026-01-21 20:34:00 +0100

    Edited

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -80,7 +80,7 @@ const Header = () => {
           </a>
 
           {/* LINKS */}
-          <div className="flex items-center gap-0.25 lg:gap-2">
+          <div className="flex items-center gap-0.05 lg:gap-2">
             {["Home", "Services", "About", "Contact"].map((item) => (
               <a
                 key={item}
