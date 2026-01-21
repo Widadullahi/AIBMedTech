0b5e09fa364c438112091f3778ae152ccb0e191b
Widadullahi <alagbewidad814@gmail.com>
2026-01-21 20:25:38 +0100

    Edited

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -81,12 +81,6 @@ const Header = () => {
 
           {/* LINKS */}
           <div className="flex items-center gap-0.5 lg:gap-2">
-            {/* Mobile top bar icons - hidden on desktop */}
-            <div className="flex md:hidden gap-1">
-              <Phone className="w-4 h-4 text-navy-700" />
-              <Mail className="w-4 h-4 text-navy-700" />
-            </div>
-
             {["Home", "Services", "About", "Contact"].map((item) => (
               <a
                 key={item}
